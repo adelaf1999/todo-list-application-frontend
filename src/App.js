d6e85c06776b234954e593fce73dfc6ba3ccfae1
Adel Abou Fakher <adelwaboufakher@gmail.com>
2022-10-07 10:28:36 -0500

    Get all tasks when component mounts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {  Button} from "react-bootstrap";
 import axios from "axios";
 import _ from "lodash";
+const BACKEND_URL = "http://localhost:3000";
+const GET_ALL_TASKS_ROUTE = `${BACKEND_URL}/get-all-tasks`;
 
 class App extends Component{
 
     constructor(props) {
 
         super(props);
+
+        const width = window.innerWidth;
+
+        const height = window.innerHeight;
+
+        this.state = {
+            width,
+            height
+        };
+
+    }
+
+    getAllTasks(){
+
+        const config = {
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        axios.get(GET_ALL_TASKS_ROUTE, config)
+            .then(response => {
+
+                const data = response.data;
+
+                const tasks = data.tasks;
+
+                console.log(tasks);
+
+
+            }).catch(error => {
+
+                console.log(error);
+
+        });
+
+    }
+
+    componentDidMount(){
+
+        this.getAllTasks();
+
 
     }
 
@@ -17,6 +61,36 @@ class App extends Component{
       return(
 
           <div>
+
+
+              <div style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  marginTop: '3rem',
+                  marginBottom: '3rem'
+              }}>
+                  <p style={{
+                      fontSize: '40px',
+                      fontWeight: 'bold',
+                      marginBottom: '1rem'
+                  }}>
+                      Tasks
+                  </p>
+
+
+                  <Button
+                      variant="success"
+                      style={{
+                          borderRadius: '10px'
+                      }}
+                  >
+                      Create Task
+                  </Button>
+
+              </div>
+
 
 
 
